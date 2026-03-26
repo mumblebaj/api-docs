adc88198281b814ef6205bb753009672c4583358
mumblebaj <mumblebaj@users.noreply.github.com>
2026-03-26 18:21:28 +0000

    deploy: ed7d7e5cf47f34206123018d2a2b42620f7f3ff6

--- js/xsdStudio.js
+++ js/xsdStudio.js
@@ -1,3 +1,3 @@
-import { bootstrapXsdStudio } from "./xsd/studio/bootstrap.js?v=20260326T150459Z";
+import { bootstrapXsdStudio } from "./xsd/studio/bootstrap.js?v=20260326T182126Z";
 
 bootstrapXsdStudio();
